fc2208b58cf3e129815aca1a01d3538cd69a0aa9
chaentopia <cce523@hanmail.net>
2023-11-27 14:43:41 +0900

    :tada: Project setting
    MVVM 연습을 위한 프로젝트 생성

---
+++ MVVM_Practice/MVVM_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Practice
+//
+//  Created by 정채은 on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
